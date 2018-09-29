a9216bb8c22c9ec3c0efbc4e91fb6552bc1ffd81
Dan Gilbert <daniel.gilbert0051@gmail.com>
2018-09-29 17:09:50 -0400

    pre component split

--- src/stream-test.js
+++ src/stream-test.js
@@ -4,45 +4,52 @@ import 'firebase/firestore';
 import db from './firebase'
 
 
-// const myId = Math.floor(Math.random()*1000000000);
-const myId = 'dan';
-const servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'webrtc','username': 'websitebeaver@mail.com'}]};
+const myId = Math.floor(Math.random()*1000000000);
+// const myId = 'dan';
+const servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'webrtc','username': 'javier3@gmail.com'}]};
 const pc = new RTCPeerConnection(servers);// console.log('database: ', db)
+console.log('PC HERE: ', pc)
 
 class StreamTest extends Component {
-constructor() {
-  super()
-  this.sendMessage = this.sendMessage.bind(this)
-  this.readMessage = this.readMessage.bind(this)
-  // this.showMyFace = this.showMyFace.bind(this)
-  this.showFriendsFace = this.showFriendsFace.bind(this)
-}
-componentDidMount() {
-  const myVideo = document.getElementById("myVideo");
-  const friendsVideo = document.getElementById("friendsVideo");
+  constructor() {
+    super()
+    this.sendMessage = this.sendMessage.bind(this)
+    this.readMessage = this.readMessage.bind(this)
+    // this.showMyFace = this.showMyFace.bind(this)
+    this.showFriendsFace = this.showFriendsFace.bind(this)
+  }
+  componentDidMount() {
+    const myVideo = document.getElementById("myVideo");
+    const friendsVideo = document.getElementById("friendsVideo");
 
-  pc.onicecandidate = (event => event.candidate?this.sendMessage(myId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-  pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+    pc.onicecandidate = (event => event.candidate?this.sendMessage(myId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+    pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
 
-  navigator.mediaDevices.getUserMedia({audio:true, video:true})
-  .then(stream => myVideo.srcObject = stream)
-  .then(stream => pc.addStream(stream));
+    //Show my face
+    navigator.mediaDevices.getUserMedia({audio:true, video:true})
+    .then(stream => myVideo.srcObject = stream)
+    .then(stream => pc.addStream(stream));
 
 
-  db.collection('users').doc(myId.toString()).onSnapshot( (doc) => {
-    console.log("Current data: ", doc.data().message);
-    // this.readMessage()
-  });
-}
+    db.collection('users').doc('dan').onSnapshot( (doc) => {
+      console.log("Current data: ", doc.data().message);
+      this.readMessage(doc);
+    });
+  }
 
   sendMessage(senderId, data) {
-    const msg = db.collection('users').doc(senderId.toString()).set({message: data})
+    const msg = db.collection('users').doc('dan').set({
+      message: data,
+      sender: senderId
+    })
     // msg.remove();
   }
 
-  readMessage(data) {
-    const msg = JSON.parse(data.val().message);
-    const sender = data.val().sender;
+  readMessage(info) {
+    console.log('in readMessage')
+    const msg = JSON.parse(info.data().message);
+    const sender = info.data().sender;
+    // const sender = 'dan';
     if(sender != myId) {
     if(msg.ice != undefined)
       pc.addIceCandidate(new RTCIceCandidate(msg.ice));
