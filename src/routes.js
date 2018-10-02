b649ed694af0880b2595625fb298fc11b4def761
Javier Carey <javier3@gmail.com>
2018-10-02 09:21:54 -0700

    adding some comments to routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//withRouter only necessary if you want access to history
 import { withRouter, Route, Switch } from 'react-router-dom';
 import Viewer from './viewer';
 import Streamer from './streamer';
@@ -6,7 +7,7 @@ import Streamer from './streamer';
 // class Routes extends Component {
 //   render() {
 //     return (
-//     <Switch>
+//     <Switch> // Switch only necessary to handle if routes should match exactly or
 //       <Route exact path='/viewer' component={Viewer} />
 //       <Route exact path='/streamer' component={Streamer} />
 //     </Switch>
@@ -15,10 +16,11 @@ import Streamer from './streamer';
 // }
 
 const Routes = () => (
-  <Switch>
+  <Switch> // Switch only necessary to handle if routes should match exactly or
     <Route exact path="/viewer" component={Viewer} />
     <Route exact path="/streamer" component={Streamer} />
   </Switch>
 );
 
+//withRouter only necessary if you want access to history
 export default withRouter(Routes);
