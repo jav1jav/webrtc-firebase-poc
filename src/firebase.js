3aee9a8235eea08072aadf341e6e2b83ea5e1129
Javier Carey <javier3@gmail.com>
2019-05-30 14:13:54 -0700

    BUGFIX: Firebase imports only products in use - Clean up index.html Fixed import statements in firebase.js to only import what we're using in the project Removed old firebase config object from index.html.

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
-import * as firebase from 'firebase';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app';
+// Firebase products in use by the project
 import 'firebase/firestore';
-// import '../secrets'
 
+// Firebase config object
 var config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,7 +13,6 @@ var config = {
   // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
-console.log('firebase.js | NODE_ENV = ', process.env.NODE_ENV, 'REACT_APP_FIREBASE_PROJECT_ID', process.env.REACT_APP_FIREBASE_PROJECT_ID, 'config = ', config)
 const settings = { timestampsInSnapshots: true };
 firebase.initializeApp(config);
 const db = firebase.firestore();
