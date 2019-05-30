72197e3d79511150b0d81f5f33c8b66378a3b9fc
Javier Carey <javier3@gmail.com>
2019-05-30 13:20:16 -0700

    BUGFIX/FEATURE: Added .env file and then gitignored it to hide secrets React apps have a convention for putting environment variables in a .env file, so what had worked in previous non-react apps using a gitignored secrets.js file wouldn't work here. I created the .env file, and then gitignored it so the secrets wouldn't show up on github. I added the environment_variables.env file so that when i later check out the code from gh that I can figure out what happened to the .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+// import '../secrets'
 
 var config = {
-  apiKey: 'AIzaSyBkAi8JlAuOEfCqFjBxOvVeXi83OwOvzoQ',
-  authDomain: 'jamspace-01.firebaseapp.com',
-  databaseURL: 'https://jamspace-01.firebaseio.com',
-  projectId: 'jamspace-01',
-  storageBucket: 'jamspace-01.appspot.com',
-  messagingSenderId: '1073650211164'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
+console.log('firebase.js | NODE_ENV = ', process.env.NODE_ENV, 'REACT_APP_FIREBASE_PROJECT_ID', process.env.REACT_APP_FIREBASE_PROJECT_ID, 'config = ', config)
 const settings = { timestampsInSnapshots: true };
 firebase.initializeApp(config);
 const db = firebase.firestore();
