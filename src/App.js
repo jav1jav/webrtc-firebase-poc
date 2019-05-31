f00e9636a25c7190ab5df082f4d6038410b6f52c
Javier Carey <javier3@gmail.com>
2019-05-31 14:39:07 -0700

    BUGFIX: Image in app, moved to HomePage component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage';
 import Routes from './routes';
@@ -8,9 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
         <HomePage />
         <Routes />
       </div>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from './logo.svg';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => (
   <div>
     <h3>Testing WebRTC Connections</h3>
+    <header className="HomePage-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     <nav>
       <Link to="/streamer">Streamer</Link> <br></br>
       <Link to="/viewer">Viewer</Link><br></br>
