b89b0c5d661ab52464c729e87000ef4d398600f8
Javier Carey <javier3@gmail.com>
2019-06-12 16:07:24 -0700

    FEATURE: Edited the write function to make it DRY-er:

--- src/utilities.js
+++ src/utilities.js
@@ -18,29 +18,64 @@ const iceWriter = (count, candidate) => {
 let streamerIceCounter = 0
 
 export const writeToFirebase = (id, field, value) => {
-  switch (field) {
-    case 'offer': {
-      return db
-        .collection('users')
-        .doc(id)
-        .set({ offer: value });
-    }
-    case 'answer': {
-      return db
-        .collection('users')
-        .doc(id)
-        .set({ answer: value }, {merge: true});
-    }
-    case 'ice': {
-      return db
-        .collection('users')
-        .doc(id)
-        .set(iceWriter(++streamerIceCounter, value), { merge: true });
-    }
-    default: {
-      console.log('default value in switch for writeToFirebase');
-    }
+  let obj = {}
+
+  if ( !(field === 'offer') ) {
+    obj['merge'] = true
   }
+  if ( field === 'ice' ) {
+    obj[field + ++streamerIceCounter] = value
+  } else {
+    obj[field] = value
+  }
+
+
+  // switch (field) {
+  //   case 'offer': {
+  //     return obj[field] = value
+  //   }
+  //   case 'answer': {
+  //     obj['merge'] = true
+  //     return obj[field] = value
+  //   }
+  //   case 'ice': {
+  //     obj['merge'] = true
+  //     return obj[field + ++streamerIceCounter] = value
+  //   }
+  //   default: {
+  //     console.log('default value in switch for writeToFirebase');
+  //   }
+  // }
+
+  return db
+    .collection('users')
+    .doc(id)
+    .set(obj)
+
+
+  // switch (field) {
+  //   case 'offer': {
+  //     return db
+  //       .collection('users')
+  //       .doc(id)
+  //       .set({ offer: value });
+  //   }
+  //   case 'answer': {
+  //     return db
+  //       .collection('users')
+  //       .doc(id)
+  //       .set({ answer: value }, {merge: true});
+  //   }
+  //   case 'ice': {
+  //     return db
+  //       .collection('users')
+  //       .doc(id)
+  //       .set(iceWriter(++streamerIceCounter, value), { merge: true });
+  //   }
+  //   default: {
+  //     console.log('default value in switch for writeToFirebase');
+  //   }
+  // }
 }
 
 // * HELPER - READ
